19b04548294c3e7ae09e9654f050963ae3aea997
Laura Dalmolin <lauraadalmolin@icloud.com>
2021-07-31 15:09:51 -0300

    feat: import do módulo de usuários e remoção do código anterior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
