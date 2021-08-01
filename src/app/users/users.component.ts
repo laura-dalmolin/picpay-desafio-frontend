cea563ba340deb068d83d8f38be425f94c4c1083
Laura Dalmolin <lauraadalmolin@icloud.com>
2021-08-01 19:16:19 -0300

    feat: tratamento de erro e criação de função para abrir dialog

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 import { User } from '../shared/interfaces/user.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { PaymentDialogComponent } from '../payment-dialog/payment-dialog.component';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  users: User[] = [];
-
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    public matDialog: MatDialog) {}
 
   ngOnInit() {
     const id = '5d531c4f2e0000620081ddce';
-    this.usersService.getUsers(id)
-      .subscribe(res => {
-        if (res.status === 200) {
-          this.users = res.body as User[];
-        }
-      });
+    this.usersService.getUsers(id).subscribe((res) => {
+      if (res.status === 200) {
+        this.users = res.body as User[];
+      }
+    }, err => {
+      console.log(err);
+    });
   }
 
+  openPaymentDialog(user: User) {
+    this.matDialog.open(PaymentDialogComponent, {
+      data: user,
+    });
+  }
 }
