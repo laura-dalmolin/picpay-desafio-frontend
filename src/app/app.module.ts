508418f9c427b184c4f7a2188d125554ef5b0e7b
Laura Dalmolin <lauraadalmolin@icloud.com>
2021-08-01 19:17:06 -0300

    refactor: organização do app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material';
+import { MessageDialogComponent } from './message-dialog/message-dialog.component';
+import { PaymentDialogComponent } from './payment-dialog/payment-dialog.component';
+import { PipeModule } from './shared/pipes/pipe.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UsersModule } from './users/users.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, MessageDialogComponent, PaymentDialogComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    UsersModule
+    CurrencyMaskModule,
+    MatDialogModule,
+    MatIconModule,
+    PipeModule,
+    ReactiveFormsModule,
+    UsersModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [MessageDialogComponent, PaymentDialogComponent],
+  providers: [PipeModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
