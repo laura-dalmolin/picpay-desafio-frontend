6eb016b8e4c5e08b1ceb983cb83f59c6c463b816
Laura Dalmolin <lauraadalmolin@icloud.com>
2021-08-01 19:19:23 -0300

    feat: import do PipeModule

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PipeModule } from '../shared/pipes/pipe.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
   ],
   exports: [
     UsersComponent
   ],
   imports: [
     CommonModule,
-    HttpClientModule
-  ]
+    HttpClientModule,
+    PipeModule,
+  ],
+  providers: [ PipeModule ]
 })
 export class UsersModule { }
